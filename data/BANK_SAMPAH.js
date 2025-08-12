8178e1266c0a0ec1817a05f27a859c225da42652
bumijokampung <bumijokampung@gmail.com>
2025-08-12 11:09:48 +0700

    Add files via upload

---
+++ data/BANK_SAMPAH.js
@@ -0,0 +1,10 @@
+var BANK_SAMPAH_JSON = {
+"type": "FeatureCollection",
+"name": "BANK_SAMPAH",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::32749" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 429719.604277606995311, 9139511.851103955879807 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 429671.869510139338672, 9139127.875654568895698 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 429632.943443171330728, 9139388.739027926698327 ] } }
+]
+};
---
+++ data/TITIK_SAMPAH.js
@@ -0,0 +1,15 @@
+var TITIK_SAMPAH_JSON = {
+"type": "FeatureCollection",
+"name": "TITIK_SAMPAH",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::32749" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 429508.733558369276579, 9139529.693312421441078 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 429548.569594040862285, 9139491.046412141993642 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 429533.705401625949889, 9139386.10521369241178 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 429288.148942933243234, 9139396.658790310844779 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 429291.716349112801254, 9139345.228684555739164 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 429710.143365589668974, 9139115.576911747455597 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 429760.681619800045155, 9139308.365487365052104 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 429761.573471345065627, 9139363.808925073593855 ] } }
+]
+};
